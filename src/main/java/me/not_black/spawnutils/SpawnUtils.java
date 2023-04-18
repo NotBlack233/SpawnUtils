d4c64a53855702bf9bf7c36f0c73881e7297c00d
NotBlack233 <unconfigured@null.spigotmc.org>
2023-04-18 13:53:07 +0800

    Initial commit

---
+++ src/main/java/me/not_black/spawnutils/SpawnUtils.java
@@ -0,0 +1,17 @@
+package me.not_black.spawnutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
